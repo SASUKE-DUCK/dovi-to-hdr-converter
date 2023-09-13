14dfe9ec34101a9cded1c53a5d2cf622f48d66fd
Sasuke-Duck UwU <95662926+SASUKE-DUCK@users.noreply.github.com>
2023-09-13 09:16:11 -0500

    Update DoVi.py

--- DoVi.py
+++ DoVi.py
@@ -52,15 +52,15 @@ dv_to_hdr10 = [
     "-filter_hw_device", "vulkan",
     "-i", input_file,
     "-vf",
-    "hwupload,libplacebo=iw:ih:format=yuv420p10le:colorspace=bt2020nc:color_primaries=bt2020:color_trc=arib-std-b67,hwdownload,format=yuv420p10le",
+    "hwupload,libplacebo=peak_detect=false:colorspace=9:color_primaries=9:color_trc=16:range=tv:format=yuv420p10le,hwdownload,format=yuv420p10le",
     "-c:v", "libx265",
     "-c:a", "copy",
-    "-x265-params",
     '-map_chapters', '-1',
     '-an',
     '-sn',
     "-y",
     '-b:v', f'{bitrate}k',
+    "-x265-params",
     f"repeat-headers=1:sar=1:hrd=1:aud=1:open-gop=0:hdr10=1:sao=0:rect=0:cutree=0:deblock=-3-3:strong-intra-smoothing=0:chromaloc=2:aq-mode=1:vbv-maxrate=160000:vbv-bufsize=160000:max-luma=1023:max-cll=0,0:master-display=G(8500,39850)B(6550,23000)R(35400,15650)WP(15635,16450)L(10000000,1)WP(15635,16450)L(1000000,100%)",
     output_path + ".mp4"
 ]
@@ -77,12 +77,12 @@ dv_to_sdr = [
     "hwupload,libplacebo=peak_detect=false:colorspace=bt709:color_primaries=bt709:color_trc=bt709:range=tv:format=yuv420p10le,hwdownload,format=yuv420p10le",
     "-c:v", "libx265",
     "-preset", preset,
-    "-x265-params",
     '-map_chapters', '-1',
     '-an',
     '-sn',
     "-y",
     '-b:v', f'{bitrate}k',
+    "-x265-params",
     "deblock=-3-3:vbv-bufsize=62500:vbv-maxrate=50000:fast-pskip=0:dct-decimate=0:level=5.1:ref=5:psy-rd=1.05,0.15:subme=11:me=umh:me_range=48",
     output_path + ".mp4"
 ]
